2fd31c6d8cd2e20a1b2c019e04fbb020b56c48d6
iamtylerd <tylerdanielphotography@gmail.com>
2016-10-25 12:54:30 -0500

    log image obj

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,14 +1,20 @@
+const fs = require('fs')
 const User = require('../models/user');
+const Photo = require('../models/photo');
+const Event = require('../models/event');
+let sF = require('../factories/storageFactory')
 
 module.exports.photo = (req, res, err) => {
 	let id = req.params.id
 	let photo = req.body.image
-	User
-		.findByIdAndUpdate(id, {
-			$push: {
-				photos: photo
-			}
-		}, {new: true})
+	console.log(req.body)
+	// User
+	// 	.findByIdAndUpdate(id, {
+	// 		$push: {
+	// 			photos: photo
+	// 		}
+	// 	}, {new: true})
+
 		.then((obj) => {
 			res.json(obj)
 		})
