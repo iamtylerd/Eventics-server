82e38b37a9f5baa1c9f1986bec1a69090464950e
iamtylerd <tylerdanielphotography@gmail.com>
2016-10-26 09:28:33 -0500

    up

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,10 @@
 const { Router } = require('express')
 const router = Router ()
 const User = require('../models/user')
-const multiparty = require('connect-multiparty')
-const multipartyMiddleware = multiparty();
 
 
 router.use(require('./loginRoute'))
-router.use(multipartyMiddleware);
+
 router.use(require('./userRoute'))
 
 module.exports = router;
