c35196146a585bf16f5108beecf63047f2abb6c8
iamtylerd <tylerdanielphotography@gmail.com>
2016-10-25 21:50:05 -0500

    sending stream

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -4,8 +4,10 @@ const Photo = require('../models/photo');
 const Event = require('../models/event');
 const sF = require('../factories/storageFactory')
 const zlib = require('zlib');
-const S3 = require('aws-sdk').S3;
-const S3S = require('s3-streams');
+const AWS = require('aws-sdk');
+
+// const S3 = require('aws-sdk').S3;
+// const S3S = require('s3-streams');
 
 let params = {
 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -16,17 +18,17 @@ let params = {
 module.exports.photo = (req, res, err) => {
 	let id = req.params.id
 	let photo = req.body.image
-	S3S.WriteStream(new S3(), {
-    Bucket: process.env.S3_BUCKEt,
-    Key: process.env.AWS_ACCESS_KEY_ID,
-});
+// 	S3S.WriteStream(new S3(), {
+//     Bucket: process.env.S3_BUCKEt,
+//     Key: process.env.AWS_ACCESS_KEY_ID,
+// });
 
 	// Can send a buffer or string
-	// var body = fs.createReadStream(req).pipe(zlib.createGzip());
-	// var s3obj = new AWS.S3(params);
-	// s3obj.upload({Body: body}).
-	//   on('httpUploadProgress', function(evt) { console.log(evt); }).
-	//   send(function(err, data) { console.log(err, data) });
+	var body = fs.createReadStream(req).pipe(zlib.createGzip());
+	var s3obj = new AWS.S3(params);
+	s3obj.upload({Body: body}).
+	  on('httpUploadProgress', function(evt) { console.log(evt); }).
+	  send(function(err, data) { console.log(err, data) });
 
 
 	// console.log(req)
