5f0075e0df95ff1878f2e742d7326722a8850751
iamtylerd <tylerdanielphotography@gmail.com>
2016-10-26 11:11:40 -0500

    can upload to s3

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -44,7 +44,7 @@ let s3Bucket = new AWS.S3({params: {
 
 
 module.exports.photo = (req, res, err) => {
-	s3Bucket.upload({Body: req, Key: uuid.v4() + '.jpg', ACL: 'public-read'}).send((err, data) => {
+	s3Bucket.upload({Body: req.result, Key: uuid.v4() + '.jpg', ACL: 'public-read'}).send((err, data) => {
 		// res.send(err || data.Location)
 		console.log(err || data.Location)
 		let url = data.Location
