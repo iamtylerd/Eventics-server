0d34ea640809871c145c5f716060a5c4ff0125a7
iamtylerd <tylerdanielphotography@gmail.com>
2016-10-27 13:03:32 -0500

    added event route

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -2,7 +2,7 @@ const { Router } = require('express')
 const router = Router()
 const event = require('../controllers/eventCtrl')
 
-router.post('/api/event/new', user.create)
+router.post('/api/event/new', event.create)
 
 
 module.exports = router
