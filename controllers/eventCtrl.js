45f2a9e1291a6b539bc8a42a470b929e4ac42cf5
iamtylerd <tylerdanielphotography@gmail.com>
2016-10-27 21:06:08 -0500

    sending photo obj

--- controllers/eventCtrl.js
+++ controllers/eventCtrl.js
@@ -28,7 +28,7 @@ module.exports.create = (req, res, err) => {
 		})
 }
 module.exports.photo = (req, res, err) => {
-	console.log(req)
+	console.log(req.IncomingMessage._parsedUrl.Url.query)
 	let name = uuid.v4() + '.jpg'
 	s3Bucket.upload({Body: req, Key: name, ACL: 'public-read'}).send((err, data) => {
 		// res.send(err || data.Location)
