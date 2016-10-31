653b6b74f12543b43c8f1a6b218a7ea35a9edcd4
iamtylerd <tylerdanielphotography@gmail.com>
2016-10-31 09:43:11 -0500

    limiting photos sent back

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -45,7 +45,7 @@ module.exports.sendEventPhotos = (req, res, err) => {
 		Photo
 		.find({
 			eventId: req.body.id
-		}).sort({date: 1}).skip(count).limit(10),
+		}).sort({date: -1}).skip(count).limit(10),
 		Event
 		.find({
 			_id: req.body.id
